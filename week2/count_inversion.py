c931353b08ef0974a87185aca71e3dc23f1ff8e8
jasonwang1031 <ywang17@babson.edu>
2017-07-18 00:55:31 -0400

    Change

---
+++ week2/count_inversion.py
@@ -0,0 +1,54 @@
+global result
+
+def mergesort(x):
+    if len(x) <= 1:
+        return x,0
+    else:
+        middle = int(len(x)/2)
+        a,C1 = mergesort(x[:middle])
+        b,C2 = mergesort(x[middle:])
+
+        C3 = sortcount(a,b)
+        c = merge(a,b)
+
+        return c,(C1+C2+C3)
+
+
+def merge(a,b):
+    c = []
+    while len(a) >0 and len(b)>0:
+        if a[0] > b[0]:
+            c.append(b[0])
+            b.remove(b[0])
+        else:
+            c.append(a[0])
+            a.remove(a[0])
+    else:
+        c += a
+        c += b
+    
+    return c
+
+def sortcount(a,b):
+    q = 0
+    w = 0
+    count = 0
+    while q < len(a):
+        while w <len(b):
+            if a[q] > b[w]:
+                count+= 1 
+                w += 1
+            else:
+                w+=1
+        w = 0
+        q += 1
+    return count
+
+
+
+
+
+
+x = [1,3,5,2,4,6]
+c,count = mergesort(x)
+print(count)
