862cca7fad1692f495fdd76b362371457ac378b9
jasonwang1031 <ywang17@babson.edu>
2017-07-23 21:19:10 -0400

    QuickSort_Improved

--- week3/QuickSort.py
+++ week3/QuickSort.py
@@ -1,24 +1,28 @@
 import random
 
-def QuickSort(A, N):
-    if N <= 1:
+def QuickSort(A):
+    if len(A) <= 1:
         return A
     else:
-        P = random.randint(0, (N-1))
-        A = Partition(A,P)
-        B = QuickSort(A[:P], len(A[:P]))
-        C = QuickSort(A[P:],len(A[P:]))
+     #   P = random.randint(0, (N-1))
+        A, P = Partition(A)
+        Pivot = A[P]
+        B = QuickSort(A[:P])
+        C = QuickSort(A[P+1:])
+        B.append(Pivot)
         array = B + C
         return array
     
 
 
 
-def Partition(A, P):
-    r = len(A)-1
+def Partition(A):
+    n = len(A)
+    r = n-1
+    p = random.randint(0, (n-1))
     j = 1
     i = 1
-    A[0], A[P] = A[P], A[0]
+    A[0], A[p] = A[p], A[0]
     while j <= r:
         if A[j] < A[0]:
             A[i], A[j] = A[j], A[i]
@@ -27,10 +31,12 @@ def Partition(A, P):
         else:
             j+=1
     A[0],A[i-1] = A[i-1],A[0]
-    return A
+    
+    return A,i-1
 
 
-A = [3,1,5,4,6,8,9]
-N = 7
-array = QuickSort(A,N)
+
+A = [3,1,5,4,6,8,9,4,6,0,3,2,1,5]
+#N = 7
+array = QuickSort(A)
 print(array)
